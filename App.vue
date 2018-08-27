e2062f816685aafdc2a8fb2a96715f9b2b20a301
niyorn <c.niyornram@hotmail.com>
2018-08-27 11:09:01 +0200

    test

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <span>lololol</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+}
+
+console.log('lol')
+</script>
+
+<style>
+
+</style>
+
---
+++ src/script.js
@@ -0,0 +1,10 @@
+import App from './App'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
